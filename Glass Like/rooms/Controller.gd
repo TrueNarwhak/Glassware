3e16d0a55d6bbdbbb4ddb001886ed3cfe6ba0822
TrueNarwhak <eodinh26@gmail.com>
2022-03-19 20:07:35 -0600

    Mouse Capture

--- Glass Like/rooms/Controller.gd
+++ Glass Like/rooms/Controller.gd
@@ -4,9 +4,7 @@ extends Node2D
 func _ready():
 	# OPTIONS INIT
 	# todo - options global
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-
+	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
 
 func _process(delta):
 	pass
