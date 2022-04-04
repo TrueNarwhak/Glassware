b351bafd477b6cbcc91a521ee92c910b0d894704
TrueNarwhak <eodinh26@gmail.com>
2022-04-04 15:21:39 -0600

    added line to pin to make ability more obvious

--- Glass Like/objects/Item Objects/PinProjectile.gd
+++ Glass Like/objects/Item Objects/PinProjectile.gd
@@ -2,8 +2,12 @@ extends Area2D
 
 export var fallspeed = 30
 var motion = Vector2.ZERO
+var show_aim = false
 
 onready var pin_cast = $PinCast
+onready var line = $Line
+
+onready var player = get_parent().get_node("Player")
 
 func _ready():
 	pass
@@ -12,6 +16,8 @@ func _process(delta):
 	if !pin_cast.is_colliding():
 		position.y += fallspeed
 	
+	line.visible = show_aim
+	line.rotation = player.motion.angle()
 
 func _on_Hitbox_body_entered(body):
 	var this_body = body.get_parent()
@@ -27,3 +33,13 @@ func _on_PinheadHitbox_area_entered(area):
 	if area.is_in_group("SlashAttack"):
 		print("A")
 		area.get_parent().can_attack_boost = true
+
+
+func _on_AimArea_body_entered(body):
+	if body.is_in_group("Player"):
+		show_aim = true
+
+
+func _on_AimArea_body_exited(body):
+	if body.is_in_group("Player"):
+		show_aim = false
