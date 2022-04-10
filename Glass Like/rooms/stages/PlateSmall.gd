426fd4d03ae69e4255c733c55ddcfb1e13b1a319
TrueNarwhak <eodinh26@gmail.com>
2022-04-10 15:42:37 -0600

    Smaller Plate

---
+++ Glass Like/rooms/stages/PlateSmall.gd
@@ -0,0 +1,5 @@
+extends Plate
+
+func _ready():
+	var bumped_eyes = preload("res://images/enemies/Plate Small/Asset 112.png") 
+	var normal_eyes = preload("res://images/enemies/Plate Small/Asset 112.png") 
