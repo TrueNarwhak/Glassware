4a0c79df8aa24a4dbada942e556150418a7be3e2
TrueNarwhak <eodinh26@gmail.com>
2022-04-19 16:59:36 -0600

    Run stats item fix

--- Glass Like/rooms/Menus/RunStats.gd
+++ Glass Like/rooms/Menus/RunStats.gd
@@ -1,11 +1,20 @@
 extends Node2D
 
+onready var item_hud = get_parent().get_node("ItemHud")
+
 onready var anim = $Control/AnimationPlayer
+onready var slot_large0 = $Control/FadeIn/SlotLarge0
+onready var slot_large1 = $Control/FadeIn/SlotLarge1
+onready var slot_large2 = $Control/FadeIn/SlotLarge2
 
 var can_respawn = false
 
 func _ready():
 	anim.play("ComeIn")
+	
+	slot_large0.texture = item_hud.slot0.texture
+	slot_large1.texture = item_hud.slot1.texture
+	slot_large2.texture = item_hud.slot2.texture
 
 func _process(delta):
 	if Input.is_action_just_pressed("restart") and can_respawn:
