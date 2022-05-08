9151a0b3057a2ad470298b5031515b9f5dc67fc7
TrueNarwhak <eodinh26@gmail.com>
2022-05-07 19:16:00 -0600

    fullscreen

--- Glass Like/rooms/Controller.gd
+++ Glass Like/rooms/Controller.gd
@@ -8,3 +8,12 @@ func _ready():
 func _process(delta):
 	if Input.is_action_just_pressed("pause"):
 		get_tree().reload_current_scene()
+	
+	# Full Screen
+	if Input.is_action_just_pressed("toggle_fullscreen"):
+		OS.window_fullscreen = !OS.window_fullscreen
+	
+	if OS.window_fullscreen:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
