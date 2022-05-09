3448cafed917440e41e0567df9d797602fba1b34
TrueNarwhak <eodinh26@gmail.com>
2022-05-09 17:11:24 -0600

    Beachball features
    resets jump, disables player collision to avoid bouncing around on shoot

--- Glass Like/objects/Beachball.gd
+++ Glass Like/objects/Beachball.gd
@@ -3,6 +3,8 @@ extends RigidBody2D
 export var ball_force = 750
 export var player_force = 750
 
+onready var hitbox = $Area2D
+
 var motion = Vector2(0.0, 0.0)
 
 func _ready():
@@ -19,9 +21,16 @@ func _on_Area2D_body_entered(body):
 	var this_body = body.get_parent()
 	
 	if body.is_in_group("Player"):
-#		body.motion.x = rand_range(-1000, 1000)
-#		body.motion.y = rand_range(-1000, 1000)
 		body.motion = position.direction_to( -get_viewport().get_mouse_position()) * player_force
+		body.can_attack_boost = true
 	
 	if this_body.is_in_group("Enemies"):
 		this_body.survive -= 1
+
+func _on_Timer_timeout():
+	set_collision_layer(1)
+	set_collision_mask(1)
+	
+	hitbox.set_collision_layer(1)
+	hitbox.set_collision_mask(1)
+
