c0690267a88d9ca6e4948e478be532d20e7fa340
TrueNarwhak <eodinh26@gmail.com>
2022-07-25 19:41:09 -0600

    Export 7

---
+++ Glass Like/global scripts/volume.gd
@@ -0,0 +1,5 @@
+extends Node
+var current_volume = 4
+
+func _ready():
+	pass
---
+++ Glass Like/objects/VolumeControls.gd
@@ -0,0 +1,48 @@
+extends Node2D
+
+var volume_ticks = 5
+var volume_sets = [-80, -40, -25, -10, 0]
+
+func _ready():
+	pass
+
+func _process(delta):
+	
+	if Volume.current_volume == 0:
+		$VolumeIcon/bar.show()
+	else:
+		$VolumeIcon/bar.hide()
+	
+	if Volume.current_volume == 1:
+		$VolumeIcon/bar2.show()
+	else:
+		$VolumeIcon/bar2.hide()
+	
+	if Volume.current_volume == 2:
+		$VolumeIcon/bar3.show()
+	else:
+		$VolumeIcon/bar3.hide()
+	
+	if Volume.current_volume == 3:
+		$VolumeIcon/bar4.show()
+	else:
+		$VolumeIcon/bar4.hide()
+		
+	if Volume.current_volume == 4:
+		$VolumeIcon/bar5.show()
+	else:
+		$VolumeIcon/bar5.hide()
+	
+	
+	if Input.is_action_just_pressed("vol_up"):
+		Volume.current_volume += 1
+		$AnimationPlayer.play("Volume")
+	
+	if Input.is_action_just_pressed("vol_down"):
+		Volume.current_volume -= 1
+		$AnimationPlayer.play("Volume")
+	
+	Volume.current_volume = clamp(Volume.current_volume, 0, 4)
+	
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), volume_sets[Volume.current_volume])
+	
