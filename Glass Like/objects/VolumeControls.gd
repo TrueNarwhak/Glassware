f62ef4f00f5d9706c4263e1bb8dab5133f98a04a
TrueNarwhak <eodinh26@gmail.com>
2022-07-30 14:27:17 -0600

    Volume Icon FIx

--- Glass Like/objects/VolumeControls.gd
+++ Glass Like/objects/VolumeControls.gd
@@ -10,25 +10,39 @@ func _process(delta):
 	
 	if Volume.current_volume == 0:
 		$VolumeIcon/bar.show()
+		$VolumeIcon/bar2.show()
 	else:
 		$VolumeIcon/bar.hide()
 	
 	if Volume.current_volume == 1:
+		$VolumeIcon/bar.show()
 		$VolumeIcon/bar2.show()
+		$VolumeIcon/bar3.show()
 	else:
 		$VolumeIcon/bar2.hide()
 	
 	if Volume.current_volume == 2:
+		$VolumeIcon/bar.show()
+		$VolumeIcon/bar2.show()
 		$VolumeIcon/bar3.show()
+		$VolumeIcon/bar4.show()
 	else:
 		$VolumeIcon/bar3.hide()
 	
 	if Volume.current_volume == 3:
+		$VolumeIcon/bar.show()
+		$VolumeIcon/bar2.show()
+		$VolumeIcon/bar3.show()
 		$VolumeIcon/bar4.show()
+		$VolumeIcon/bar5.show()
 	else:
 		$VolumeIcon/bar4.hide()
 		
 	if Volume.current_volume == 4:
+		$VolumeIcon/bar.show()
+		$VolumeIcon/bar2.show()
+		$VolumeIcon/bar3.show()
+		$VolumeIcon/bar4.show()
 		$VolumeIcon/bar5.show()
 	else:
 		$VolumeIcon/bar5.hide()
@@ -36,10 +50,12 @@ func _process(delta):
 	
 	if Input.is_action_just_pressed("vol_up"):
 		Volume.current_volume += 1
+		$AnimationPlayer.stop()
 		$AnimationPlayer.play("Volume")
 	
 	if Input.is_action_just_pressed("vol_down"):
 		Volume.current_volume -= 1
+		$AnimationPlayer.stop()
 		$AnimationPlayer.play("Volume")
 	
 	Volume.current_volume = clamp(Volume.current_volume, 0, 4)
